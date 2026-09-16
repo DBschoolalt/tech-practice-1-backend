e206f4a489e7501efc1a3359b577f0e62473be1d
forealZees <boichukdenisshcoolalt@gmail.com>
2026-09-16 12:00:18 +0200

    rough sqlite support

--- src/item_data.ts
+++ src/item_data.ts
@@ -1,17 +1,30 @@
 import fs from 'fs';
+import { DatabaseSync } from 'node:sqlite';
 import { Item } from "./item.js"
 
 export class ItemData {
-
+	static db = new DatabaseSync('data/item_data.db')
 	static data_location: string = "./data/item_data.json"
 
 	static read(): Item[] {
+		// ItemData.db.exec(`CREATE TABLE IF NOT EXISTS item_data (
+		// 	id INTEGER PRIMARY KEY AUTOINCREMENT,
+		// 	name TEXT NOT NULL,
+		// 	desc TEXT NOT NULL,
+		// 	image_path TEXT NOT NULL,
+		// 	price FLOAT NOT NULL,
+		// 	amount INTEGER NOT NULL
+		// )`)
+
 		try {
-			let data = fs.readFileSync(ItemData.data_location, 'utf8');
-			console.log('Success reading JSON file for Item Data');
-			return JSON.parse(data)
+			// let data2 = fs.readFileSync(ItemData.data_location, 'utf8');
+			// console.log('Success reading JSON file for Item Data');
+
+			const data = ItemData.db.prepare(`SELECT * FROM item_data`);
+			console.log('Success reading DB file for Item Data');
+			return JSON.parse(JSON.stringify(data.all()))
 		} catch (error) {
-			console.error('Error reading JSON file:', error);
+			console.error('Error reading DB file:', error);
 		}
 		return []
 		
@@ -19,12 +32,19 @@ export class ItemData {
 
 	static write(data: Item[]): void {
 		try {
-			fs.writeFileSync(ItemData.data_location, JSON.stringify(data), 'utf8');
-			console.log('Success writing JSON file for Item Data');
+			//fs.writeFileSync(ItemData.data_location, JSON.stringify(data), 'utf8');
+			//console.log('Success writing JSON file for Item Data');
+
+			ItemData.db.exec('DELETE FROM item_data')
+			ItemData.db.exec('VACUUM')
+			for (const item of data) {
+				ItemData.db.exec(`INSERT INTO item_data (id, name, desc, image_path, price, amount) VALUES (${item.id}, '${item.name}', '${item.desc}', '${item.image_path}', ${item.price}, ${item.amount})`);	
+			}
+			console.log('Success writing DB file for Item Data');
+
 		} catch (error) {
-			console.error('Error writing JSON file:', error);
+			console.error('Error writing DB file:', error);
 		}
 	}
 }
-
 
